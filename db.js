12156deae5f5fb3cbcab013300b84f5ae5e4e5da
al1211 <kanil013623@gmail.com>
2025-07-12 16:28:28 +0530

    add mongodb

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ const mongoUrl=process.env.MONGO_URI;
 const ConnectDb=async()=>{
     try{
         await mongoose.connect(mongoUrl);
+        console.log(mongoUrl);
         console.log("mongodb connect");
     }catch(err){
-        console.error("mongoose not connect");
+        console.error("mongoose not connect",err.messaage);
         process.exit(1);
     }
 };
